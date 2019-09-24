eb31fe140e54bb9fa683447e8501804866b0b03e
nianshao <1902436747@qq.com>
2019-09-24 22:43:55 +0800

    Site updated: 2019-09-24 22:43:47

--- dist/music.js
+++ dist/music.js
@@ -7,7 +7,7 @@ const ap = new APlayer({
     {
       name: "偏爱",
       artist: '张芸京',
-      url: 'http://win.web.ra03.sycdn.kuwo.cn/9f664df8c076bdfa1647e03a6d67f9b6/5d85bc6d/resource/a1/48/92/11/2975233388.aac',
+      url: 'http://music.163.com/song/media/outer/url?id=86369',
       cover: 'http://star.kuwo.cn/star/starheads/180/24/49/1438549359.jpg',
       lrc: 'LRC/许巍-无尽光芒.lrc',
     },
@@ -20,14 +20,14 @@ const ap = new APlayer({
     {
       name: '	此生不换仙剑奇侠传三 电视剧原声带',
       artist: '青鸟飞鱼',
-      url: 'http://182.247.250.21/amobile.music.tc.qq.com/C400000zrtvQ3BZOQo.m4a?guid=1459971536&vkey=AD2CAC4B7A971F1B3426A8FD25D18362AF2AE0BBBBAA489D923CC82FCF0AAE59BE593CD75BE2903E806F1B85F957F9AD356AE1A293EDE1BC&uin=0&fromtag=68',
-      cover: 'https://s2.ax1x.com/2019/08/08/e7M5VK.png',
+      url: 'http://music.163.com/song/media/outer/url?id=86363',
+      cover: 'http://p2.music.126.net/2-_sPmCBnN6VfwFmONRy2Q==/46179488378975.jpg?param=130y130',
     },
     {
       name: '忘记时间仙剑奇侠传三 电视剧原声带',
       artist: '胡歌',
-      url: 'http://106.117.253.28/amobile.music.tc.qq.com/C400003OBhAU0cI7gk.m4a?guid=1459971536&vkey=A5F0657241698B7DB36D24EA44096EE41D3791494BC89FDAABA8896D5B32127FBDCF918711E37E541474E9CA5F948CAC759311D6A0C039B5&uin=0&fromtag=68',
-      cover: 'https://s2.ax1x.com/2019/08/08/e7M5VK.png',
+      url: 'http://music.163.com/song/media/outer/url?id=86360',
+      cover: 'http://p2.music.126.net/2-_sPmCBnN6VfwFmONRy2Q==/46179488378975.jpg?param=130y130',
     },
     // ,
     // {
@@ -42,17 +42,35 @@ const ap = new APlayer({
       url: 'http://music.163.com/song/media/outer/url?id=1365898499',
       cover: 'http://p1.music.126.net/Bq6Io8lpY1l2HsQ28QKFlw==/109951164083996255.jpg?param=130y130',
     },
-{
+   {
       name: '圈住你',
       artist: '巧乐兹',
       url: 'http://music.163.com/song/media/outer/url?id=1388357890',
       cover: 'http://p2.music.126.net/adU6WbJI4uZMpfkPXIMyQQ==/109951164359250906.jpg?param=130y130',
     },
-{
+   {
       name: '画',
       artist: '王巨星',
       url: 'http://music.163.com/song/media/outer/url?id=1379416556',
       cover: 'http://p1.music.126.net/XoOjqWvL3MUZIDADjoNacA==/109951164347163273.jpg?param=130y130',
+    },
+    {
+      name: '太阳 (完整版) (Cover：邱振哲)',
+      artist: '程佳佳',
+      url: 'http://music.163.com/song/media/outer/url?id=1387178379',
+      cover: 'http://p2.music.126.net/g0XBgjDBKm6aTC1IIPYFgA==/109951164328580208.jpg?param=130y130',
+    },
+    {
+      name: 'Sansa Lala',
+      artist: 'lala',
+      url: 'http://music.163.com/song/media/outer/url?id=1327781623',
+      cover: 'http://p2.music.126.net/Jl9DEf4VGLaBi_DEyaJqPw==/109951163779294836.jpg?param=130y130',
+    },
+    {
+      name: '一直很安静',
+      artist: '阿桑',
+      url: 'http://music.163.com/song/media/outer/url?id=5260494',
+      cover: 'http://p2.music.126.net/Zyji-kiWpjLF19CF0yHSlg==/1727332767246198.jpg?param=130y130',
     }
 
   ]
