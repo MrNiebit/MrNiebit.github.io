1aee789e16e739377b90a8c557dfcc8531a73101
nianshao <1902436747@qq.com>
2019-10-30 23:07:25 +0800

    Site updated: 2019-10-30 23:07:17

---
+++ js/src/jitang.js
@@ -0,0 +1,3 @@
+// build time:Wed Oct 30 2019 23:07:17 GMT+0800 (CST)
+(function(){$.ajax({url:"http://66.lacknb.cn:9527/jitang",method:"GET",dataType:"jsonp",success:function(o){$("#drugwords").text(o.data);console.log(o)},jsonpCallback:"callback"});function o(){this.r=Math.floor(Math.random()*255);this.g=Math.floor(Math.random()*255);this.b=Math.floor(Math.random()*255);return"rgba("+this.r+","+this.g+","+this.b+",0.5)"+"!important"}var t=o();console.log(t);$("#drugwords").css("color",t)})();
+//rebuild by neat 
