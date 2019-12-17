5830c5840221a58c165e6c3f4efaf0711693b0a4
nianshao <1902436747@qq.com>
2019-12-17 22:45:36 +0800

    Site updated: 2019-12-17 22:45:32

--- static/api/js/share/combine_api.js
+++ static/api/js/share/combine_api.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 17 2019 15:29:21 GMT+0800 (CST)
+// build time:Tue Dec 17 2019 22:45:31 GMT+0800 (CST)
 window._bd_share_main.F.module("share/combine_api",function(n,a,i){var e=n("base/tangram").T,s=n("base/class").Class,o=n("share/api_base");a.CombineApi=s.create(function(n){var a=this,i=null,e=null;a.setApi=function(n,a){i=n,e=a},a._init=function(){i&&e&&i.on("sharecompleted",function(n){})}},o.ApiBase)});
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 17 2019 15:29:21 GMT+0800 (CST)
+// build time:Tue Dec 17 2019 22:45:31 GMT+0800 (CST)
 
 //rebuild by neat 
