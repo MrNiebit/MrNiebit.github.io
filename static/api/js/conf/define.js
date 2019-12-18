2ba45c2dfee3102e75cd6e95b91644ddc729c557
nianshao <1902436747@qq.com>
2019-12-18 23:31:36 +0800

    Site updated: 2019-12-18 23:31:22

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 17 2019 22:45:31 GMT+0800 (CST)
+// build time:Wed Dec 18 2019 23:17:18 GMT+0800 (CST)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
--- static/api/js/trans/data.js
+++ static/api/js/trans/data.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 17 2019 22:45:31 GMT+0800 (CST)
+// build time:Wed Dec 18 2019 23:17:18 GMT+0800 (CST)
 
 //rebuild by neat 
