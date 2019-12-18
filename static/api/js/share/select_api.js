8d2dcbba70233647b8c923fd2e0a78f792da314e
nianshao <1902436747@qq.com>
2019-12-19 00:14:46 +0800

    Site updated: 2019-12-19 00:14:44

--- static/api/js/share/select_api.js
+++ static/api/js/share/select_api.js
@@ -1,3 +1,3 @@
-// build time:Wed Dec 18 2019 23:49:08 GMT+0800 (CST)
+// build time:Thu Dec 19 2019 00:14:43 GMT+0800 (CST)
 window._bd_share_main.F.module("share/select_api",function(e,a,n){var t=e("base/tangram").T,i=e("base/class").Class,s=e("component/comm_tools"),o=e("share/api_base");a.Api=i.create(function(e){var a=this;a._init=function(){var e=a.getView();e.render(),e.init()},a._processAction=function(e){return{data:{type:"select"}}},a._distory=function(){}},o.ApiBase)});
 //rebuild by neat 
